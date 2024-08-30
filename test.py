17ba5bbadf5baa59276d85a46183f8d2edff8391
deyvivek03 <vivek.dey@modak.com>
2024-08-30 16:06:59 +0530

    latest update

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, You are using Vivek's image.........."
+    return "Hello, You are using Vivek's image...... Thankyou"
 
 
 if __name__ == '__main__':
